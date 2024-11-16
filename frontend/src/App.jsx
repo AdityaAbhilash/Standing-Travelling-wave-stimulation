20d183ffd17348ce355f0c370b8c3fcb59018965
aditya <abhilashaditya77@gmail.com>
2024-11-16 13:19:19 +0530

    deployment2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Router } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <Router basename="/Standing-Travelling-wave-stimulation">
     <div className="App">
       <Outlet />
       </div>
+      </Router>
   )
 }
 
